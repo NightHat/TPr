9cb012bb308e914ac4b053b2794c5412dd3eb503
Karina <kararin1331@gmail.com>
2017-04-28 09:31:47 +0300

    HelloWorld!

---
+++ Lab1/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    class CMessageBox
+    {
+        public static void Show(String str)
+        {
+            MessageBox.Show(str);
+        }
+    };
+    class App 
+    {
+        public static void Main()
+        {
+            CMessageBox.Show("Hello, World!");
+        }
+    }
+}
