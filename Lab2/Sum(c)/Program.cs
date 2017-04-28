778ffe050fcfbda7e097041295fc544d804eb7a5
Karina <kararin1331@gmail.com>
2017-04-28 09:58:22 +0300

    Sum

---
+++ Lab2/Sum(c)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double a;
+            double b;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Произведение этих чисел: " + (a + b).ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
